ef8479a11b46006da1ffe71008d2797d19134f34
Arsaide <arsaidekm@gmail.com>
2024-10-29 00:19:31 +0200

    detail statistics
    - worked at charts hs library
    - added more logic and graphs

--- frontend-cinema/src/components/ui/admin/admin-statistics/middle-statistics/top-movies/TopMoviesLoading.tsx
+++ frontend-cinema/src/components/ui/admin/admin-statistics/middle-statistics/top-movies/TopMoviesLoading.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import Loader from '@/components/ui/Loader';
+
+import styles from './TopMovies.module.scss';
+
 const TopMoviesLoading = () => {
-	return <div></div>;
+	return (
+		<div className={styles.top_movies}>
+			<div className={'h-[390px] w-full flex items-center justify-center'}>
+				<Loader />
+			</div>
+		</div>
+	);
 };
 
 export default TopMoviesLoading;
