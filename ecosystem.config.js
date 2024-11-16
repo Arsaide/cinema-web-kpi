04a80ae6bd1791369e8ed5b8119318759d5a2262
Arsaide <arsaidekm@gmail.com>
2024-11-17 01:40:03 +0200

    setting yml action file and pm2 config
    - added ecosystem.config.ts this is pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,24 @@
 module.exports = {
   apps: [
     {
-      name: "cinema-mono-repo",
-      script: "yarn",
-      args: "turbo start",
-      interpreter: "node",
+      name: "turbo-build",
+      script: "npx",
+      args: "turbo build",
       cwd: "./",
-      autorestart: true,
       watch: false,
+      log_file: "./logs/turbo-build.log",
+      error_file: "./logs/turbo-build-error.log",
+      time: true,
+    },
+    {
+      name: "turbo-start",
+      script: "npx",
+      args: "turbo start",
+      cwd: "./",
+      watch: false,
+      log_file: "./logs/turbo-start.log",
+      error_file: "./logs/turbo-start-error.log",
+      time: true,
     },
   ],
 };
